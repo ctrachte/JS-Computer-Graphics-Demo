e7483cf879c0972628022e7149a40b35dcaac052
Caleb Trachte <cetrachte@hotmail.com>
2021-04-14 13:35:57 -0500

    refactored to switch statement for performance/framerate

--- rayTrace.js
+++ rayTrace.js
@@ -11,15 +11,15 @@ const canvas_pitch = canvas_buffer.width * 4;
 
 
 // The PutPixel() function.
-const PutPixel = function(x, y, color) {
-  x = canvas.width/2 + x;
-  y = canvas.height/2 - y - 1;
+const PutPixel = function (x, y, color) {
+  x = canvas.width / 2 + x;
+  y = canvas.height / 2 - y - 1;
 
   if (x < 0 || x >= canvas.width || y < 0 || y >= canvas.height) {
     return;
   }
 
-  let offset = 4*x + canvas_pitch*y;
+  let offset = 4 * x + canvas_pitch * y;
   canvas_buffer.data[offset++] = color[0];
   canvas_buffer.data[offset++] = color[1];
   canvas_buffer.data[offset++] = color[2];
@@ -28,7 +28,7 @@ const PutPixel = function(x, y, color) {
 
 
 // Displays the contents of the offscreen buffer into the canvas.
-const UpdateCanvas = function() {
+const UpdateCanvas = function () {
   canvas_context.putImageData(canvas_buffer, 0, 0);
 }
 
@@ -38,13 +38,13 @@ const UpdateCanvas = function() {
 // ======================================================================
 
 // Dot product of two 3D vectors.
-const DotProduct = function(v1, v2) {
-  return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2];
+const DotProduct = function (v1, v2) {
+  return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 }
 
 
 // Computes v1 - v2.
-const Subtract = function(v1, v2) {
+const Subtract = function (v1, v2) {
   return [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]];
 }
 
@@ -54,13 +54,13 @@ const Subtract = function(v1, v2) {
 // ======================================================================
 
 // A Sphere.
-const Sphere = function(center, radius, color) {
+const Sphere = function (center, radius, color) {
   this.center = center;
   this.radius = radius;
   this.color = color;
 }
 
-function getSpheres () {
+function getSpheres() {
   return spheres;
 }
 
@@ -70,40 +70,40 @@ let projection_plane_z = 1;
 let camera_position = [0, 0, 0];
 let background_color = [255, 255, 255];
 let spheres = [new Sphere([0, -1, 3], 1, [255, 0, 0]),
-           new Sphere([2, 0, 4], 1, [0, 0, 255]),
-           new Sphere([-2, 0, 4], 1.5, [0, 0, 0])];
+new Sphere([2, 0, 4], 1, [0, 0, 255]),
+new Sphere([-2, 0, 4], 1.5, [0, 0, 0])];
 
 
 // Converts 2D canvas coordinates to 3D viewport coordinates.
-const CanvasToViewport = function(p2d) {
+const CanvasToViewport = function (p2d) {
   return [p2d[0] * viewport_size / canvas.width,
-      p2d[1] * viewport_size / canvas.height,
-      projection_plane_z];
+  p2d[1] * viewport_size / canvas.height,
+    projection_plane_z];
 }
 
 
 // Computes the intersection of a ray and a sphere. Returns the values
 // of t for the intersections.
-const IntersectRaySphere = function(origin, direction, sphere) {
+const IntersectRaySphere = function (origin, direction, sphere) {
   let oc = Subtract(origin, sphere.center);
 
   let k1 = DotProduct(direction, direction);
-  let k2 = 2*DotProduct(oc, direction);
-  let k3 = DotProduct(oc, oc) - sphere.radius*sphere.radius;
+  let k2 = 2 * DotProduct(oc, direction);
+  let k3 = DotProduct(oc, oc) - sphere.radius * sphere.radius;
 
-  let discriminant = k2*k2 - 4*k1*k3;
+  let discriminant = k2 * k2 - 4 * k1 * k3;
   if (discriminant < 0) {
     return [Infinity, Infinity];
   }
 
-  let t1 = (-k2 + Math.sqrt(discriminant)) / (2*k1);
-  let t2 = (-k2 - Math.sqrt(discriminant)) / (2*k1);
+  let t1 = (-k2 + Math.sqrt(discriminant)) / (2 * k1);
+  let t2 = (-k2 - Math.sqrt(discriminant)) / (2 * k1);
   return [t1, t2];
 }
 
 
 // Traces a ray against the set of spheres in the scene.
-const TraceRay = function(origin, direction, min_t, max_t) {
+const TraceRay = function (origin, direction, min_t, max_t) {
   let closest_t = Infinity;
   let closest_sphere = null;
   let spheres = getSpheres();
@@ -132,9 +132,9 @@ const TraceRay = function(origin, direction, min_t, max_t) {
 //
 // setInterval(Main, 1);
 
-function Main () {
-  for (let x = -canvas.width/2; x < canvas.width/2; x++) {
-    for (let y = -canvas.height/2; y < canvas.height/2; y++) {
+function Main() {
+  for (let x = -canvas.width / 2; x < canvas.width / 2; x++) {
+    for (let y = -canvas.height / 2; y < canvas.height / 2; y++) {
       let direction = CanvasToViewport([x, y])
       let color = TraceRay(camera_position, direction, 1, Infinity);
       PutPixel(x, y, color);
@@ -146,24 +146,29 @@ function Main () {
 Main();
 
 window.addEventListener("keyup", function (e) {
-  if (e.key === "PageUp") {
-    spheres[0].center[2] -= 0.2*(-1);
-    Main();
-  } else if (e.key === "w") {
-    spheres[0].center[1] -= 0.2*(-1);
-    Main();
-  } else if (e.key === "PageDown") {
-    spheres[0].center[2] += 0.2*(-1);
-    Main();
-  } else if (e.key === "s") {
-    spheres[0].center[1] += 0.2*(-1);
-    Main();
-  } else if (e.key === "a") {
-    spheres[0].center[0] += 0.2*(-1);
-    Main();
-  } else if (e.key === "d") {
-    spheres[0].center[0] -= 0.2*(-1);
-    Main();
+  let keypress = e.key;
+  switch (keypress) {
+    case "w":
+      spheres[0].center[1] -= 0.2 * (-1);
+      break;
+    case "a":
+      spheres[0].center[0] += 0.2 * (-1);
+      break;
+    case "s":
+      spheres[0].center[1] += 0.2 * (-1);
+      break;
+    case "d":
+      spheres[0].center[0] -= 0.2 * (-1);
+      break;
+    case "PageDown":
+      spheres[0].center[2] += 0.2 * (-1);
+      break;
+    case "PageUp":
+      spheres[0].center[2] -= 0.2 * (-1);
+      break;
+    default:
+      break;
   }
+  Main();
 });
 
